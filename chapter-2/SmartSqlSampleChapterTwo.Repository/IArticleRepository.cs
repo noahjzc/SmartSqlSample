6e7abc634ea8ec58ea2ece71397ce69f6587dc92
Noah.Ji <noah_jzc@126.com>
2019-05-14 10:26:10 +0800

    u

--- chapter-2/SmartSqlSampleChapterTwo.Repository/IArticleRepository.cs
+++ chapter-2/SmartSqlSampleChapterTwo.Repository/IArticleRepository.cs
@@ -1,9 +1,17 @@
 ﻿using SmartSql.DyRepository;
+using SmartSql.DyRepository.Annotations;
 using SmartSqlSampleChapterTwo.Entity;
+using System.Data;
 
 namespace SmartSqlSampleChapterTwo.Repository
 {
+    [SqlMap(Scope = "CustomScope")]
     public interface IArticleRepository : IRepository<T_Article, long>
     {
+        [Statement(CommandType = CommandType.Text, Execute = ExecuteBehavior.ExecuteScalar, Id = "Offline")]
+        void OfflineArticle([Param("Id", FieldType = typeof(long))] long articleId);
+
+        [Statement(Sql = "Update T_Article Set Status = 1 Where Id = @Id")]
+        void OnlineArticle([Param("Id")] long article);
     }
 }
